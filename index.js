28b06654c6875443693a5f7a80d6c594d1ab7449
jaydeepkhatri <jaydeepkhatri79@gmail.com>
2023-02-11 10:35:08 +0530

    ➕ Add Post method

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3001;
-const data = require("./data/data")
+const data = require("./data/data");
+
+const app = express();
 
 app.get('/v1', (req, res) => {
     res.set('Access-Control-Allow-Origin', '*');
@@ -13,7 +15,19 @@ app.get('/v1/:id', (req, res) => {
     res.set('Access-Control-Allow-Origin', '*');
     let quote = data[parseInt(req.params.id) - 1];
     res.json(quote);
-})
+});
+
+app.post('/v1', (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    let randomQuote = data[Math.floor(Math.random() * data.length)];
+    res.json(randomQuote);
+});
+
+app.post('/v1/:id', (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    let quote = data[parseInt(req.params.id) - 1];
+    res.json(quote);
+});
 
 app.listen(3000, () => {
     console.log('server listening to port 3000')
